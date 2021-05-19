036d725f87b5891955bc3814f58983ecb5fa847a
Liu-Steve <73787075+Liu-Steve@users.noreply.github.com>
2021-05-20 02:42:49 +0800

    upload6
    一些任务可以进行了，要求放在TODO_list.md里

--- CircuitSimulator_in_Cpp/circuitmap.cpp
+++ CircuitSimulator_in_Cpp/circuitmap.cpp
@@ -7,18 +7,53 @@
   * bug:(when one bug was fixed, marked it with '~')
   *     null
   * TODO:(when one was completed, marked it with '~')
-  *     null
+  *     1.Finish the show-all button.
+  *     2.Complete the actions of components, like put-on , delete and move.
+  *     3.Matrix of the can-put points.
   */
 #include "circuitmap.h"
 #include "ui_circuitmap.h"
 #include <QPainter>
 #include <QDebug>
+#include <QGraphicsView>
+#include <QSlider>
+
+//画可放元件的点阵
+void CircuitMap::drawDots_Map()
+{
+    QPainter painter(this);
+    QPen pen(QColor(200, 200, 200));
+    pen.setWidth(2);
+    painter.setPen(pen);
+}
 
 CircuitMap::CircuitMap(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::CircuitMap)
 {
     ui->setupUi(this);
+
+    //设置滑动条与数值框
+    ui->slider_Zoom->setRange(10, 100);
+    ui->slider_Zoom->setSingleStep(10);
+    ui->slider_Zoom->setTickInterval(30);
+    ui->slider_Zoom->setTickPosition(QSlider::TicksAbove);
+    ui->slider_Zoom->setValue(50);
+    ui->spinBox_Zoom->setRange(10, 100);
+    ui->spinBox_Zoom->setSingleStep(10);
+    ui->spinBox_Zoom->setValue(50);
+    connect(ui->slider_Zoom, &QSlider::valueChanged, ui->spinBox_Zoom, [=](){
+        int v = ui->slider_Zoom->value();
+        v = (v + 5) / 10 * 10;
+        ui->spinBox_Zoom->setValue(v);
+        ui->slider_Zoom->setValue(v);
+        zoom = v;
+    });
+    connect(ui->spinBox_Zoom, &QSpinBox::textChanged, ui->slider_Zoom, [=](){
+        int v = ui->spinBox_Zoom->value();
+        ui->slider_Zoom->setValue(v);
+        zoom = v;
+    });
 }
 
 CircuitMap::~CircuitMap()
--- build-CircuitSimulator_in_Cpp-Desktop_Qt_5_14_2_MinGW_64_bit-Debug/ui_circuitmap.h
+++ build-CircuitSimulator_in_Cpp-Desktop_Qt_5_14_2_MinGW_64_bit-Debug/ui_circuitmap.h
@@ -11,6 +11,14 @@
 
 #include <QtCore/QVariant>
 #include <QtWidgets/QApplication>
+#include <QtWidgets/QGraphicsView>
+#include <QtWidgets/QHBoxLayout>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QSlider>
+#include <QtWidgets/QSpacerItem>
+#include <QtWidgets/QSpinBox>
+#include <QtWidgets/QVBoxLayout>
 #include <QtWidgets/QWidget>
 
 QT_BEGIN_NAMESPACE
@@ -18,12 +26,67 @@ QT_BEGIN_NAMESPACE
 class Ui_CircuitMap
 {
 public:
+    QVBoxLayout *verticalLayout;
+    QGraphicsView *graphicsView;
+    QWidget *widget;
+    QHBoxLayout *horizontalLayout;
+    QLabel *label_Zoom;
+    QSpinBox *spinBox_Zoom;
+    QSlider *slider_Zoom;
+    QSpacerItem *horizontalSpacer;
+    QPushButton *btn_Biggest;
 
     void setupUi(QWidget *CircuitMap)
     {
         if (CircuitMap->objectName().isEmpty())
             CircuitMap->setObjectName(QString::fromUtf8("CircuitMap"));
-        CircuitMap->resize(400, 300);
+        CircuitMap->resize(800, 600);
+        CircuitMap->setMinimumSize(QSize(800, 600));
+        verticalLayout = new QVBoxLayout(CircuitMap);
+        verticalLayout->setObjectName(QString::fromUtf8("verticalLayout"));
+        graphicsView = new QGraphicsView(CircuitMap);
+        graphicsView->setObjectName(QString::fromUtf8("graphicsView"));
+
+        verticalLayout->addWidget(graphicsView);
+
+        widget = new QWidget(CircuitMap);
+        widget->setObjectName(QString::fromUtf8("widget"));
+        horizontalLayout = new QHBoxLayout(widget);
+        horizontalLayout->setObjectName(QString::fromUtf8("horizontalLayout"));
+        label_Zoom = new QLabel(widget);
+        label_Zoom->setObjectName(QString::fromUtf8("label_Zoom"));
+
+        horizontalLayout->addWidget(label_Zoom);
+
+        spinBox_Zoom = new QSpinBox(widget);
+        spinBox_Zoom->setObjectName(QString::fromUtf8("spinBox_Zoom"));
+
+        horizontalLayout->addWidget(spinBox_Zoom);
+
+        slider_Zoom = new QSlider(widget);
+        slider_Zoom->setObjectName(QString::fromUtf8("slider_Zoom"));
+        QSizePolicy sizePolicy(QSizePolicy::Minimum, QSizePolicy::Fixed);
+        sizePolicy.setHorizontalStretch(0);
+        sizePolicy.setVerticalStretch(0);
+        sizePolicy.setHeightForWidth(slider_Zoom->sizePolicy().hasHeightForWidth());
+        slider_Zoom->setSizePolicy(sizePolicy);
+        slider_Zoom->setMinimumSize(QSize(160, 0));
+        slider_Zoom->setOrientation(Qt::Horizontal);
+
+        horizontalLayout->addWidget(slider_Zoom);
+
+        horizontalSpacer = new QSpacerItem(40, 20, QSizePolicy::Expanding, QSizePolicy::Minimum);
+
+        horizontalLayout->addItem(horizontalSpacer);
+
+        btn_Biggest = new QPushButton(widget);
+        btn_Biggest->setObjectName(QString::fromUtf8("btn_Biggest"));
+
+        horizontalLayout->addWidget(btn_Biggest);
+
+
+        verticalLayout->addWidget(widget);
+
 
         retranslateUi(CircuitMap);
 
@@ -33,6 +96,8 @@ public:
     void retranslateUi(QWidget *CircuitMap)
     {
         CircuitMap->setWindowTitle(QCoreApplication::translate("CircuitMap", "Form", nullptr));
+        label_Zoom->setText(QCoreApplication::translate("CircuitMap", "\346\224\276\347\274\251", nullptr));
+        btn_Biggest->setText(QCoreApplication::translate("CircuitMap", "\346\230\276\347\244\272\345\205\250\351\203\250", nullptr));
     } // retranslateUi
 
 };
