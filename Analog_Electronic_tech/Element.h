e182a6867fc27ff3bc88df9783983926c3467f02
Realsaltedfish <1076105471@qq.com>
2021-05-25 16:30:26 +0800

    I don't know what I did...

--- Analog_Electronic_tech/Element.h
+++ Analog_Electronic_tech/Element.h
@@ -5,10 +5,18 @@
 #ifndef ANALOG_ELECTRONIC_TECH_ELEMENT_H
 #define ANALOG_ELECTRONIC_TECH_ELEMENT_H
 #include "pch.h"
+typedef std::pair<int, int>pin;
 
 class Element {
 private:
-
+    int ID;
+    int Type;
+    double V_pin1;
+    double V_pin2;
+    double I_1to2;
+    double R;
+    pin pin1;
+    pin pin2;
 public:
     
 };
