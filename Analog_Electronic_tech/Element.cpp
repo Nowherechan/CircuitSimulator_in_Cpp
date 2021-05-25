ab02cd9c65adea43b4ccea32a8acc935c9bb8d91
Realsaltedfish <1076105471@qq.com>
2021-05-25 18:09:18 +0800

    Finished the Element Class...I am vegetable...

--- Analog_Electronic_tech/Element.cpp
+++ Analog_Electronic_tech/Element.cpp
@@ -3,3 +3,45 @@
 //
 
 #include "Element.h"
+using std::endl;
+using std::cout;
+using std::istream;
+const double EPS = 1e-10;
+
+Element::Element(int id, int type, double v_pin1, double v_pin2, double i_1to2,
+                 double r, int p1_x, int p1_y, int p2_x, int p2_y)
+{
+    ID = id;
+    Type = type;
+    V_pin1 = v_pin1;
+    V_pin2 = v_pin2;
+    I_1to2 = i_1to2;
+    R = r;
+    pin1.first = p1_x;
+    pin1.second = p1_y;
+    pin2.first = p2_x;
+    pin2.second = p2_y;
+}
+
+int Element::get_ID() const { return ID; }
+
+int Element::get_Type() const { return Type; }
+
+pin Element::get_pin1() { return pin1; }
+
+pin Element::get_pin2() { return pin2; }
+
+double Element::get_V_pin1() const { return V_pin1; }
+
+double Element::get_V_pin2() const { return V_pin2; }
+
+double Element::get_I_1to2() const { return I_1to2; }
+
+double Element::get_R() const { return R; }
+
+void Element::modify_I_1to2(double in) { I_1to2 = in; }
+
+void Element::modify_V_pin1(double in) { V_pin1 = in; }
+
+void Element::modify_V_pin2(double in) { V_pin2 = in; }
+
--- Analog_Electronic_tech/Element.h
+++ Analog_Electronic_tech/Element.h
@@ -7,8 +7,8 @@
 #include "pch.h"
 typedef std::pair<int, int>pin;
 
-class Element {
-private:
+class Element {         // 電路元件
+private:                // 電源視爲理想電源，故R爲0；未賦值時初始值爲-1
     int ID;
     int Type;
     double V_pin1;
@@ -18,7 +18,19 @@ private:
     pin pin1;
     pin pin2;
 public:
-    
+    Element (int ID, int Type, double V_pin1, double V_pin2,
+             double I_1to2, double R, int p1_x, int p1_y, int p2_x, int p2_y);  //構造, (x, y)表示坐標
+    pin get_pin1();                                                             //返回成員變量
+    pin get_pin2();
+    double get_V_pin1() const;
+    double get_V_pin2() const;
+    double get_I_1to2() const;
+    double get_R() const;
+    int get_Type() const;
+    int get_ID() const;
+    void modify_V_pin1 (double in);                                             //修改值
+    void modify_V_pin2 (double in);
+    void modify_I_1to2 (double in);
 };
 
 
