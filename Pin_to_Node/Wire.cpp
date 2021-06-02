acef5d8d7d2cd955f966c0f239db2bbb055dba9b
Realsaltedfish <zzxy233@gmail.com>
2021-06-02 21:22:48 +0800

    Finished the Wire Class

--- Pin_to_Node/Wire.cpp
+++ Pin_to_Node/Wire.cpp
@@ -3,3 +3,22 @@
 //
 
 #include "Wire.h"
+#include "pch.h"
+
+Wire::Wire(int pin1_x, int pin1_y, int pin2_x, int pin2_y)
+{
+    pin1.x = pin1_x;
+    pin1.y = pin1_y;
+    pin2.x = pin2_x;
+    pin2.y = pin2_y;
+    status = false;
+}
+
+Pin Wire::get_pin1() { return pin1; }
+
+Pin Wire::get_pin2() { return pin2; }
+
+bool Wire::get_status() const { return status; }
+
+void Wire::change_status(bool in) { status = in; }
+
--- Pin_to_Node/Wire.h
+++ Pin_to_Node/Wire.h
@@ -5,12 +5,12 @@
 #ifndef PIN_TO_NODE_WIRE_H
 #define PIN_TO_NODE_WIRE_H
 
-typedef struct {    //端點 Pin, 有坐標x, y
+typedef struct {    // 端點 Pin, 有坐標x, y
     int x;
     int y;
 }Pin;
 
-class Wire {
+class Wire {        // 初始導線電平爲 0 ，即低電平
 private:
     Pin pin1;
     Pin pin2;
@@ -19,7 +19,8 @@ public:
     Wire(int pin1_x, int pin1_y, int pin2_x, int pin2_y);
     Pin get_pin1();
     Pin get_pin2();
-    bool get_status();
+    bool get_status() const;
+    void change_status(bool in);
 };
 
 
